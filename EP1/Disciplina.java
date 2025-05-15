a86c00c58c3769a199aff0afcbfbdcdc8f5acf6c
Luccas Rodrigues dos Santos <luccasrs2005@gmail.com>
2025-05-14 21:26:32 -0300

    Adiciona a Classe "Disciplina"

--- EP1/Disciplina.java
+++ EP1/Disciplina.java
@@ -1,28 +1,17 @@
 import java.util.ArrayList;
 
 public class Disciplina {
-<<<<<<< HEAD
     private String nome; // Nome da disciplina
     private String codigo; // Código da disciplina
     private int cargaHoraria; // Carga horária da disciplina
     private ArrayList<String> preRequisitos; // Os pré-requisitos para cursar a disciplina
     private Turma turma; // As turmas que cada disciplina tem
-=======
-    private String nome;
-    private String codigo;
-    private int cargaHoraria;
-    private ArrayList<String> preRequisitos;
-    private Turma turma;
->>>>>>> 1a5e93b43467f059340589327d39a9c6989f1161
 
     public Disciplina() {
         this.nome = "";
         this.codigo = "";
         this.cargaHoraria = 0;
-<<<<<<< HEAD
         this.preRequisitos = new ArrayList<>();
-=======
->>>>>>> 1a5e93b43467f059340589327d39a9c6989f1161
     }
 
     public void setNome(String nome) {
@@ -60,8 +49,4 @@ public class Disciplina {
     public Turma geTurma() {
         return turma;
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 1a5e93b43467f059340589327d39a9c6989f1161
