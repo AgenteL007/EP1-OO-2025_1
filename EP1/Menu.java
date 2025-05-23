5f7aada24ae31bb3dc05b34b28e7d1c43636ef60
Luccas Rodrigues dos Santos <luccasrs2005@gmail.com>
2025-05-23 16:43:43 -0300

    Altera a Main ("Menu")

--- EP1/Menu.java
+++ EP1/Menu.java
@@ -4,9 +4,12 @@ public class Menu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("===== SISTEMA ACADÊMICO DA FCTE =====");
+        System.out.println("");
+        System.out.println("Seja bem-vindo ao Sistema Acadêmico!");
+        System.out.println("");
+
         while (true) { // Exibe o menu
-            System.out.println("===== SISTEMA ACADÊMICO DA FCTE =====");
-            System.out.println("Seja bem-vindo ao Sistema Acadêmico!");
             System.out.println("Por favor, escolha o modo que deseja acessar ou digite 4 para sair:");
             System.out.println("Modo 1 - Aluno (Normal e Especial)");
             System.out.println("Modo 2 - Disciplina/Turma");
@@ -20,12 +23,14 @@ public class Menu {
 
                 switch (opcaoPrincipal) { // Processa a opção escolhida
                     case 1:
+                        System.out.println("");
                         System.out.println("Modo Aluno (Normal e Especial) selecionado. Escolha uma função:");
-                        System.out.println("Função 1- Cadastrar novo aluno");
-                        System.out.println("Função 2 - Editar aluno existente");
-                        System.out.println("Função 3 - Listar alunos cadastrados no sistema");
-                        System.out.println("Função 4 - Trancar disciplina/semestre");
-                        System.out.println("5. Voltar para o menu");
+                        System.out.println("Função 1 - Cadastrar novo aluno (normal)");
+                        System.out.println("Função 2 - Cadastrar novo aluno (especial)");
+                        System.out.println("Função 3 - Editar aluno existente");
+                        System.out.println("Função 4 - Listar alunos cadastrados no sistema");
+                        System.out.println("Função 5 - Trancar disciplina/semestre");
+                        System.out.println("6. Voltar para o menu");
                         System.out.print("Acessar a função: ");
 
                         if (sc.hasNextInt()) { // Verifica se a próxima entrada é um Inteiro
@@ -38,35 +43,42 @@ public class Menu {
 
                                     break;
                                 case 2:
-                                    System.out.println("Função de edição selecionada.");
+                                    System.out.println("Função de cadastro selecionada.");
 
                                     break;
                                 case 3:
-                                    System.out.println("Listagem dos alunos cadastrados no sistema:");
+                                    System.out.println("Função de edição selecionada.");
 
                                     break;
                                 case 4:
-                                    System.out.println("Função de trancamento selecionada.");
+                                    System.out.println("Listagem dos alunos cadastrados no sistema:");
 
                                     break;
                                 case 5:
+                                    System.out.println("Função de trancamento selecionada.");
+
+                                    break;
+                                case 6:
                                     break; // Volta para o menu
                                 default:
+                                    System.out.println("");
                                     System.out.println("Opção inválida. Tente novamente.");
                             }
                         } else { // Caso não seja digitado um Inteiro
+                            System.out.println("");
                             System.out.println("Entrada inválida. Digite apenas números.");
                             sc.nextLine();
                         }
 
                         break;
                     case 2:
+                        System.out.println("");
                         System.out.println("Modo Disciplina/Turma selecionado. Escolha uma função:");
                         System.out.println("Função 1 - Cadastrar nova disciplina");
                         System.out.println("Função 2 - Criar nova turma");
                         System.out.println("Função 3 - Listar turmas"); // Mostrar também os alunos matriculados
                         System.out.println("4. Voltar para o menu");
-                        System.out.print("Acessar a função ");
+                        System.out.print("Acessar a função: ");
 
                         if (sc.hasNextInt()) { // Verifica se a próxima entrada é um Inteiro
                             int opcao2 = sc.nextInt();
@@ -88,15 +100,18 @@ public class Menu {
                                 case 4:
                                     break; // Volta para o menu
                                 default:
+                                    System.out.println("");
                                     System.out.println("Opção inválida. Tente novamente.");
                             }
                         } else { // Caso não seja digitado um Inteiro
+                            System.out.println("");
                             System.out.println("Entrada inválida. Digite apenas números.");
                             sc.nextLine();
                         }
 
                         break;
                     case 3:
+                        System.out.println("");
                         System.out.println("Modo Avaliação/Frequência selecionado. Escolha uma função:");
                         System.out.println("Função 1 - Lançar notas"); // Serão lançadas por turma e mostrará a média
                                                                        // final
@@ -153,24 +168,30 @@ public class Menu {
                                 case 9:
                                     break; // Volta para o menu
                                 default:
+                                    System.out.println("");
                                     System.out.println("Opção inválida. Tente novamente.");
 
                                     break;
                             }
                         } else { // Caso não seja digitado um Inteiro
+                            System.out.println("");
                             System.out.println("Entrada inválida. Digite apenas números.");
                             sc.nextLine();
                         }
 
                         break;
                     case 4:
+                        System.out.println("");
                         System.out.println("Sistema fechado. Tenha um bom dia!");
+                        System.out.println("");
                         sc.close();
                         return;
                     default:
+                        System.out.println("");
                         System.out.println("Opção inválida. Tente novamente.");
                 }
             } else { // Caso não seja digitado um Inteiro
+                System.out.println("");
                 System.out.println("Entrada inválida. Digite apenas números.");
                 sc.nextLine();
             }
