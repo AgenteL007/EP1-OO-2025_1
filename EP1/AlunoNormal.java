78d8e263fe3d8829589342b531c6923dda87eaa9
Luccas Rodrigues dos Santos <luccasrs2005@gmail.com>
2025-05-26 21:10:13 -0300

    Adiciona o Método de Verificar Aprovação por Nota na Subclasse "AlunoNormal"

--- EP1/AlunoNormal.java
+++ EP1/AlunoNormal.java
@@ -1,9 +1,9 @@
 public class AlunoNormal extends Aluno {
-    private int p1; // Prova 1
-    private int p2; // Prova 2
-    private int p3; // Prova 3
-    private int l; // Lista de Exercícios
-    private int s; // Seminário
+    private float p1; // Prova 1
+    private float p2; // Prova 2
+    private float p3; // Prova 3
+    private float l; // Lista de Exercícios
+    private float s; // Seminário
 
     public AlunoNormal() {
         this.p1 = 0;
@@ -13,43 +13,63 @@ public class AlunoNormal extends Aluno {
         this.s = 0;
     }
 
-    public void setP1(int p1) {
+    public void setP1(float p1) {
         this.p1 = p1;
     }
 
-    public int getP1() {
+    public float getP1() {
         return p1;
     }
 
-    public void setP2(int p2) {
+    public void setP2(float p2) {
         this.p2 = p2;
     }
 
-    public int getP2() {
+    public float getP2() {
         return p2;
     }
 
-    public void setP3(int p3) {
+    public void setP3(float p3) {
         this.p3 = p3;
     }
 
-    public int getP3() {
+    public float getP3() {
         return p3;
     }
 
-    public void setL(int l) {
+    public void setL(float l) {
         this.l = l;
     }
 
-    public int getL() {
+    public float getL() {
         return l;
     }
 
-    public void setS(int s) {
+    public void setS(float s) {
         this.s = s;
     }
 
-    public int getS() {
+    public float getS() {
         return s;
+    }
+
+    public void verificarAprovacaoSemPeso(float p1, float p2, float p3, float l, float s) {
+        float mediaFinal = (p1 + p2 + p3 + l + s) / 5;
+
+        if (mediaFinal < 5) {
+            System.out.println("Aluno reprovado por nota.");
+        } else {
+            System.out.println("Aluno com nota para passar.");
+        }
+    }
+
+    public void verificarAprovacaoComPeso(float p1, float p2, float p3, float l, float s) {
+        float mediaFinal = (p1 + p2 * 2 + p3 * 3 + l * 3 + s) / 8;
+
+        if (mediaFinal < 5) {
+            System.out.println("Aluno reprovado por nota.");
+        } else {
+            System.out.println("Aluno com nota para passar.");
+        }
     }
 }
